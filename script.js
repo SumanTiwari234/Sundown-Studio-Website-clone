c828947ea082e83037c99988b529abfb8ddb53d0
Suman Tiwari <sumantiwari12678@gmail.com>
2026-04-10 02:04:18 +0530

    added some changes

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+window.addEventListener("load", () => {
+    locoScroll.update();
+});
+
+setTimeout(() => {
+    locoScroll.update();
+}, 1500);
 
 var elemc = document.querySelector("#elem-con")
 var fixImage = document.querySelector("#fixed-image")
 
 var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
+elems.forEach(function (e) {
     var img = e.getAttribute("data-image")
-    e.addEventListener("mouseenter",function(){
+    e.addEventListener("mouseenter", function () {
         fixImage.style.display = "block"
         fixImage.style.backgroundImage = `url(${img})`
-        fixImage.addEventListener("mouseenter",function(){
+        fixImage.addEventListener("mouseenter", function () {
             fixImage.style.display = "block";
         })
     })
-    e.addEventListener("mouseleave",function(){
-        fixImage.style.display = "none" 
+    e.addEventListener("mouseleave", function () {
+        fixImage.style.display = "none"
     })
 
-    fixImage.addEventListener("mouseleave",function(){
+    fixImage.addEventListener("mouseleave", function () {
         fixImage.style.display = "none";
     })
 })
@@ -33,10 +40,10 @@ var Project = document.querySelector("#Project")
 var Execution = document.querySelector("#Execution")
 var image = document.querySelector("#page4-img")
 
-Design.addEventListener("click",function(){
+Design.addEventListener("click", function () {
     desc.textContent = "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings."
     var add = Design.getAttribute("data-img")
-    image.setAttribute("src",`${add}`)
+    image.setAttribute("src", `${add}`)
     Project.style.right = "0vw"
     Design.style.right = "2vw"
     design.style.color = "#EFEAE3"
@@ -45,10 +52,10 @@ Design.addEventListener("click",function(){
     Project.style.color = "#504A45"
 })
 
-Project.addEventListener("click",function(){
+Project.addEventListener("click", function () {
     desc.textContent = "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need."
     var add = Project.getAttribute("data-img")
-    image.setAttribute("src",`${add}`)
+    image.setAttribute("src", `${add}`)
     Project.style.right = "2vw"
     Project.style.color = "#EFEAE3"
     Design.style.right = "0vw"
@@ -57,10 +64,10 @@ Project.addEventListener("click",function(){
     Execution.style.right = "0vw"
 })
 
-Execution.addEventListener("click",function(){
+Execution.addEventListener("click", function () {
     desc.textContent = "We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience."
     var add = Execution.getAttribute("data-img")
-    image.setAttribute("src",`${add}`)
+    image.setAttribute("src", `${add}`)
     Project.style.right = "0vw"
     Design.style.right = "0vw"
     Execution.style.right = "2vw"
@@ -70,11 +77,14 @@ Execution.addEventListener("click",function(){
 })
 
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: "auto",
-      centeredSlides: true,
-      spaceBetween: 10,
-      pagination: {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 10,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-    });
+    },
+});
+
+
+
